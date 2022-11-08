c6886d3889e38ccc6db96a5da9cd717d7e4ee089
Aalbaa <asg1036@alu.ubu.es>
2022-11-08 20:08:12 +0100

    Tabla definitiva de enfemedades cardio. (Sobre estos datos realizar los gráficos...)

--- Tablas_enfermedad_cardio.R
+++ Tablas_enfermedad_cardio.R
@@ -2,11 +2,24 @@ library(readr)
 enf_cardio2_0 <- read_delim("enf_cardio2.0.csv", 
                             delim = "\t", escape_double = FALSE, 
                             col_names = FALSE, trim_ws = TRUE)
-View(enf_cardio2_0)
+
 #Los datos de la tabla vienen en tantos por 100.000
 
-#pongo nombrea a las columnas del dataframe y elimino las q no son necesarias; en este caso la del sexo (X4) y las edades (X3)
+#pongo nombre a las columnas del dataframe 
 
 colnames(enf_cardio2_0)<- c('Causa Defuncion', 'Comunidades autonomas',
                             'X3', 'X4', 'Año', 'Total')
+#elimino las columnas que NO nos interesan (X3: Edades y X4: Sexo) y la primera fila
+enf_cardio2_0$X3 <- NULL
+enf_cardio2_0$X4 <- NULL
+
+#enf_cardio2_0$Causa Defunción <-NULL   ya que sabemos q esta tabla solo contiene los datos en cuanto a enfermedades cardiovasculares
+
+enf_cardio2_0 <- enf_cardio2_0[-1,]
+
+#Como todas las columnas me aparecen tipo caracter; modifico el total como valor numérico 
+enf_cardio2_0 <- transform(enf_cardio2_0, Total=as.numeric(as.factor(Total)))
+View(enf_cardio2_0)
+
+
 

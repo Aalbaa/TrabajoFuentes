68f457c511f0b8fbd04bc2f1b7a34cdd2647ee2b
Aalbaa <asg1036@alu.ubu.es>
2022-11-08 10:34:36 +0100

    nombro las columnas del dataframe

--- Tablas_enfermedad_cardio.R
+++ Tablas_enfermedad_cardio.R
@@ -4,3 +4,9 @@ enf_cardio2_0 <- read_delim("enf_cardio2.0.csv",
                             col_names = FALSE, trim_ws = TRUE)
 View(enf_cardio2_0)
 #Los datos de la tabla vienen en tantos por 100.000
+
+#pongo nombrea a las columnas del dataframe y elimino las q no son necesarias; en este caso la del sexo (X4) y las edades (X3)
+
+colnames(enf_cardio2_0)<- c('Causa Defuncion', 'Comunidades autonomas',
+                            'X3', 'X4', 'Año', 'Total')
+

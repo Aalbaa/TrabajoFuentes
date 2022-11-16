871c05df4faf666bcf8b08e5fa734027ab08f9b8
Aalbaa <asg1036@alu.ubu.es>
2022-11-16 08:48:56 +0100

    carpeta input con la tabla de los datos más RMarkdown

--- Tablas_enfermedad_cardio.R
+++ Tablas_enfermedad_cardio.R
@@ -1,5 +1,5 @@
 library(readr)
-enf_cardio2_0 <- read_delim("enf_cardio2.0.csv", 
+enf_cardio2_0 <- read_delim("INPUT.DATA/enf_cardio2.0.csv", 
                             delim = "\t", escape_double = FALSE, 
                             col_names = FALSE, trim_ws = TRUE)
 
@@ -7,7 +7,7 @@ enf_cardio2_0 <- read_delim("enf_cardio2.0.csv",
 
 #pongo nombre a las columnas del dataframe 
 
-colnames(enf_cardio2_0)<- c('Causa Defuncion', 'Comunidades autonomas',
+colnames(enf_cardio2_0)<- c('Causa Defuncion', 'Comunidades',
                             'X3', 'X4', 'Año', 'Total')
 #elimino las columnas que NO nos interesan (X3: Edades y X4: Sexo) y la primera fila
 enf_cardio2_0$X3 <- NULL
@@ -18,8 +18,12 @@ enf_cardio2_0$X4 <- NULL
 enf_cardio2_0 <- enf_cardio2_0[-1,]
 
 #Como todas las columnas me aparecen tipo caracter; modifico el total como valor numérico 
-enf_cardio2_0 <- transform(enf_cardio2_0, Total=as.numeric(as.factor(Total)))
+enf_cardio2_0 <- transform(enf_cardio2_0, Total=as.numeric(Total))
+
 View(enf_cardio2_0)
+str(enf_cardio2_0)
 
-
+#gráfico de barras
+library(tidyverse)
+ggplot(data = enf_cardio2_0, aes(x = Comunidades, y = Total))
 
